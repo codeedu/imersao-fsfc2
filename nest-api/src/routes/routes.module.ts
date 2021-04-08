6b1f5355d589320a3ea2acc493bfa30dd78f3598
Luiz Carlos <argentinaluiz@gmail.com>
2021-04-08 14:39:36 -0300

    fix: Connection with Kafka using SASL

--- nest-api/src/routes/routes.module.ts
+++ nest-api/src/routes/routes.module.ts
@@ -20,11 +20,9 @@ import { RoutesGateway } from './routes.gateway';
               brokers: [process.env.KAFKA_BROKER],
               ssl: true,
               sasl: {
-                mechanism: 'plain',
-                plain: {
-                  username: process.env.KAFKA_SASL_USERNAME,
-                  password: process.env.KAFKA_SASL_PASSWORD,
-                },
+                mechanism: 'plain', // scram-sha-256 or scram-sha-512
+                username: process.env.KAFKA_SASL_USERNAME,
+                password: process.env.KAFKA_SASL_PASSWORD,
               },
             },
             consumer: {
