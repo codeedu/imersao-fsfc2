e30c398afeec5f2a63543660106436370c52f9b6
Wesley Willians <wesleywillians@gmail.com>
2021-04-09 00:50:52 -0400

    k8s

--- nest-api/src/routes/routes.controller.ts
+++ nest-api/src/routes/routes.controller.ts
@@ -81,6 +81,10 @@ export class RoutesController implements OnModuleInit {
       };
     },
   ) {
-    this.routeGateway.sendPosition(message.value);
+    
+  this.routeGateway.sendPosition({
+    ...message.value,
+    position: [message.value.position[1], message.value.position[0]],
+  });
   }
 }
